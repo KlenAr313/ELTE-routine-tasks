686dfbb67772ffefcecf2afc34f422f92af016db
Klenovszky Áron <h9mrbv@INF.ELTE.HU>
2022-10-11 10:13:36 +0200

    Progalap 10.11 FérfiNő

---
+++ ProgAlap/FerefiNo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FerefiNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] szemszam = new string[N];
+            int i;
+            for (i = 0; i < N; i++)
+            {
+                szemszam[i] = Console.ReadLine();
+            }
+
+            i = 0;
+            while (i < N && (szemszam[i][0] != '1' && szemszam[i][0] != '3'))
+            {
+                i++;
+            }
+
+            if (i < N)
+            {
+                Console.WriteLine("Van");
+            }
+            else
+            {
+                Console.WriteLine("Nincs");
+            }
+
+            i = 0;
+            while (i < N && szemszam[i][0] != '4')
+            {
+                i++;
+            }
+
+            Console.WriteLine(i+1);
+        }
+    }
+}
